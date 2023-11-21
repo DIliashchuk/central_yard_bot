f0f1ad2157235791c771ebb1edd25dc07728005c
dmytroiliashchuk <diliashchuk@gmail.com>
2023-11-21 16:16:55 +0200

    ddd

--- CentralYard/appointment.py
+++ CentralYard/appointment.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def staff():
+def book_staff():
     url = "https://api.alteg.io/api/v1/book_staff/777559"
 
     payload = json.dumps({
@@ -27,19 +27,18 @@ def staff():
 
     if response.status_code == 200:
         data = response.json()
-        staff_list = []
+        staff_list = {}
 
         for staff_member in data.get('data', []):
             staff_id = staff_member.get('id')
             staff_name = staff_member.get('name')
-            full_staff = staff_id, staff_name
-            staff_list.append(full_staff)
+            staff_list[staff_name] = staff_id
         return staff_list
     else:
         print("Ошибка при запросе:", response.status_code)
 
 
-print(staff())
+# print(book_staff())
 
 
 def services():
@@ -71,17 +70,93 @@ def services():
 
     if response.status_code == 200:
         data = response.json()
-        service_list = []
+        service_list = {}
 
         for staff_member in data.get('data', []):
             service_id = staff_member.get('id')
             service_name = staff_member.get('booking_title')
-            full_info = service_id, service_name
-            service_list.append(full_info)
+            service_list[service_name] = service_id
         return service_list
     else:
         print("Ошибка при запросе:", response.status_code)
 
-print(services())
+# print(services())
 
 
+def book_dates():
+    url = "https://api.alteg.io/api/v1/book_dates/777559"
+
+    payload = json.dumps({
+        "success": True,
+        "data": {
+            "booking_days": {},
+            "booking_dates": [],
+            "working_days": {},
+            "working_dates": []
+        },
+        "meta": []
+    })
+    headers = {
+        'Accept': 'application/vnd.api.v2+json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer kmhut9umky73276yryjt, User 5efa5ab2f2474930209f9392c78a3dab',
+        'Cookie': '__cf_bm=al1Mz8EUz9y4Z2WtaRueiqNKdvjU1Xdt.iKMmW7fkyk-1700568479-0-AZWOJx3Jn7Hz3glyRMNp7OmB1'
+                  'mWSm0vcgMt/Xt9VoaXXTRKcYVPbUh/G1YjTrxkvuWY7sTElZykmQsBiOcC09CU=; _ga_b1fzH1=GS1.1-2.1664391493.'
+                  '1.1.1664391604.0.0.0; _ga_dqrr=GS1.1-2.1664391493.1.1.1664391604.0.0.0; auth=kio09h8nt3u1d5im4'
+                  'dtoqeae1m904csincupd8uq7i7jijh75r25f7al0kqpf7v6'
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        booking_dates = data.get('data', {}).get('booking_dates', [])
+        return booking_dates
+    else:
+        print("Ошибка при запросе:", response.status_code)
+
+# print(book_dates())
+
+def book_times(date):
+
+    url = f"https://api.alteg.io/api/v1/book_times/777559/2219665/{date}"
+
+    payload = json.dumps({
+      "success": True,
+      "data": [
+        {},
+        {},
+        {},
+        {},
+        {}
+      ],
+      "meta": []
+    })
+    headers = {
+      'Accept': 'application/vnd.api.v2+json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer kmhut9umky73276yryjt, User 5efa5ab2f2474930209f9392c78a3dab',
+      'Cookie': '__cf_bm=WwI80tCHxMoP4ncjE3Ie9INcc.VtVDPSPRg.X7v6Kk4-1700562213-0-ASqa9wi1dsB4tfsi4i'
+                'RIMQDMzNaALOyfBbA9+/s8KR9+lRdeYAY++oJfhKxr22aErTnszNK4mE/AzB2qJhFnSts=; _ga_b1fx49=GS1.1-2.'
+                '1664391493.1.1.1664391604.0.0.0; _ga_b1fx5h=GS1.1-2.1664391493.1.1.1664391604.0.0.0; _ga_dqrr=GS1.'
+                '1-2.1664391493.1.1.1664391604.0.0.0; auth=kio09h8nt3u1d5im4dtoqeae1m904csincupd8uq7i7jijh75r'
+                '25f7al0kqpf7v6'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    if response.status_code == 200:
+        data = response.json()
+        book_list = {}
+
+        for book_time in data.get('data', []):
+            booking_time = book_time.get('time')
+            book_datetime = book_time.get('datetime')
+            book_list[booking_time] = book_datetime
+        return book_list
+    else:
+        print("Ошибка при запросе:", response.status_code)
+
+    print(response.text)
+
+# print(book_times('2023-11-25'))
