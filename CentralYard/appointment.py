f6e7821ab684babb2bee16b6695cbba8b419f1b0
boanboanboan <bessonov.bo@gmail.com>
2023-11-26 16:50:31 +0200

    new era <3 2.0

--- CentralYard/appointment.py
+++ CentralYard/appointment.py
@@ -88,10 +88,6 @@ def services(category_id):
         print("Ошибка при запросе:", response.status_code)
 
 
-# services_for_category = services(10593304)  # Передайте правильный category_id
-# print(services_for_category)
-
-
 def book_dates(staff_id):
     url = f"https://api.alteg.io/api/v1/book_dates/92533?staff_id={staff_id}"
 
@@ -128,54 +124,6 @@ def book_dates(staff_id):
         return booking_dates
     else:
         print("Ошибка при запросе:", response.status_code)
-
-
-def book_times(selected_date, staff_id):
-
-    url = f"https://api.alteg.io/api/v1/book_times/92533/{staff_id}/{selected_date}"
-
-    payload = json.dumps({
-      "success": True,
-      "data": [
-        {},
-        {},
-        {},
-        {},
-        {}
-      ],
-      "meta": []
-    })
-    headers = {
-      'Accept': 'application/vnd.api.v2+json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer m4mbf8khn2a4anacm2ej, User 6e82ffb3f03dc939f9abf65bd92292b1',
-      'Cookie': '__cf_bm=bMg.8lxa22JTb2GBXTFNgq2tLolTL1o_lv7IAh0zi5k-1700829158-0-AWQKAXHYPuNu0R02B7VFt/'
-                'NYE/K6voIr40DzHrvYRhuWPA5oEd1eebC+F7UrM2icKSYVobHrDF2N3T/3QPC1M6s=;'
-                ' _ga_b1gE8Q=GS1.1-2.1664391493.1.1.1664391604.0.0.0;'
-                ' _ga_b1gEXb=GS1.1-2.1664391493.1.1.1664391604.0.0.0;'
-                ' _ga_b1gEYQ=GS1.1-2.1664391493.1.1.1664391604.0.0.0;'
-                ' _ga_b1gFbm=GS1.1-2.1664391493.1.1.1664391604.0.0.0;'
-                ' _ga_b1gFpN=GS1.1-2.1664391493.1.1.1664391604.0.0.0;'
-                ' _ga_b1gFwV=GS1.1-2.1664391493.1.1.1664391604.0.0.0;'
-                ' _ga_yeD=GS1.1-2.1664391493.1.1.1664391604.0.0.0;'
-                ' auth=666ase19j9u8qpdoq8f7cst0elc85f2utm2310dm8ie5bea6l5pv7uh4vqob1kvh'
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    if response.status_code == 200:
-        data = response.json()
-        book_list = {}
-
-        for book_time in data.get('data', []):
-            booking_time = book_time.get('time')
-            book_datetime = book_time.get('datetime')
-            book_list[booking_time] = book_datetime
-        return book_list
-    else:
-        print("Ошибка при запросе:", response.status_code)
-
-    print(response.text)
 
 
 def finallys(service_id, staff_id, selected_date, selected_time, name, phone, email):
@@ -226,4 +174,46 @@ def finallys(service_id, staff_id, selected_date, selected_time, name, phone, em
     except http.client.HTTPException as e:
         print(f"Ошибка при выполнении запроса: {e}")
         return False
+
+
+def book_time(staff_id, selected_date):
+    url = f"https://api.alteg.io/api/v1/book_times/92533/{staff_id}/{selected_date}"
+
+    payload = json.dumps({
+        "success": True,
+        "data": [],
+        "meta": []
+    })
+    headers = {
+        'Accept': 'application/vnd.api.v2+json ',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer m4mbf8khn2a4anacm2ej, User 6e82ffb3f03dc939f9abf65bd92292b1',
+        'Cookie': '__cf_bm=GKViXQijTzlJCmZS64vuoZFcmGnn2HSsNNgeHQX8eE4-1701005662-0-AV02qqF7sdUaGIo+Y'
+                  'kEuTA/OfWVGVmByAUTWKMvpdFeIEVMz/5db9/grXV4iMBJM02uvaAILZ5K4Xnn8nZ3hkOU=; _ga_b1ho4E=GS1.'
+                  '1-2.1664391493.1.1.1664391604.0.0.0; _ga_b1ho4h=GS1.1-2.1664391493.1.1.1664391604.0.0.0;'
+                  ' _ga_b1ho6y=GS1.1-2.1664391493.1.1.1664391604.0.0.0; _ga_b1ho7j=GS1.1-2.1664391493.1.1.166'
+                  '4391604.0.0.0; _ga_b1hpcG=GS1.1-2.1664391493.1.1.1664391604.0.0.0; _ga_b1hpcT=GS1.1-2.166439'
+                  '1493.1.1.1664391604.0.0.0; _ga_b1hpcg=GS1.1-2.1664391493.1.1.1664391604.0.0.0; _ga_b1hpd9=GS1.1'
+                  '-2.1664391493.1.1.1664391604.0.0.0; _ga_b1hpdk=GS1.1-2.1664391493.1.1.1664391604.0.0.0; _ga_b1h'
+                  'pdy=GS1.1-2.1664391493.1.1.1664391604.0.0.0; _ga_b1hpeg=GS1.1-2.1664391493.1.1.1664391604.0.0.0; '
+                  '_ga_b1hprm=GS1.1-2.1664391493.1.1.1664391604.0.0.0; _ga_b1hpss=GS1.1-2.1664391493.1.1.1664391604.'
+                  '0.0.0; _ga_yeD=GS1.1-2.1664391493.1.1.1664391604.0.0.0; auth=kio09h8nt3u1d5im4dtoqeae1m904csinc'
+                  'upd8uq7i7jijh75r25f7al0kqpf7v6'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    if response.status_code == 200:
+        data = response.json()
+        time_list = []
+        for staff_member in data.get('data', []):
+            work_time = staff_member.get('time')
+            work_seance = staff_member.get('seance_length')
+            if work_seance >= 3600:
+                time_list.append(work_time)
+        return time_list
+    else:
+        print("Ошибка при запросе:", response.status_code)
+
+
 
